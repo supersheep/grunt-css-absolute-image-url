1185c6a4ca3d64f3096ebd14953692f48dd46ac9
supersheep <supersheepxjy@gmail.com>
2013-09-26 12:07:37 +0800

    underline to dash

--- tasks/css_absolute_image_url.js
+++ tasks/css_absolute_image_url.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('css_absolute_image_url', 'Your task description goes here.', function() {
+  grunt.registerMultiTask('css-absolute-image-url', 'Your task description goes here.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     
 
